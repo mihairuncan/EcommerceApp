02713b717d3cd62d2a8a267fe7ccc215f7dd75f1
Mihai <runcan.mihai@gmail.com>
2022-03-23 23:31:02 +0200

    Improved UI/UX

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -11,7 +11,7 @@ import { ShopService } from './shop.service';
   styleUrls: ['./shop.component.scss']
 })
 export class ShopComponent implements OnInit {
-  @ViewChild('search', { static: true }) searchTerm: ElementRef; // for #search input
+  @ViewChild('search', { static: false }) searchTerm: ElementRef; // for #search input
   products: IProduct[];
   brands: IBrand[];
   types: IType[];
